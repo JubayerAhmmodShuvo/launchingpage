e9825744097c4f9a70bf2ded5e30aeb0b8972c2c
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2021-09-22 23:04:26 +0600

    d

--- sc.js
+++ sc.js
@@ -1,5 +1,5 @@
 
-  var countDownDate = new Date ("Jan 16, 2022 00:00:00").getTime();
+  var countDownDate = new Date ("Nov 16, 2021 00:00:00").getTime();
   var x = setInterval(function() {
       var now = new Date().getTime();
       var distance = countDownDate - now ;
